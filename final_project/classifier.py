9ed6899b3dd01ccada991d4594cd8e70fdc071db
Markus Woodson <markus.woodson1@gmail.com>
2016-03-16 18:11:10 -0400

    data loader implemented

--- final_project/classifier.py
+++ final_project/classifier.py
@@ -13,14 +13,19 @@ Test Pre-process:
 Classification:
 	Perform KNN, SVM, or any other classification algorithm we would like to try
 '''
+import os
 import numpy as np
+import cv2
+from numpy import linalg as LA
 from sklearn.decomposition import PCA
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 
 def denoiseSilhouette(img):
 	'''
-	Denoise the image silhouettes to give a more rounded shape.
+	Denoise the image silhouettes to give a more rounded shape. Use this
+	when using real world images as background model will not be given
+	and silhouettes will likely be noisier than training data.
 	'''
 	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
 	erosion = cv2.erode(img,kernel,iterations = 2)
@@ -43,13 +48,70 @@ def findMaxContour(img):
 	cy = int(M['m01']/M['m00'])
 	return cx, cy, cnt
 
-def toDistSeq(path):
+def toDistSeq(img_path):
 	'''
 	Given the path to a folder of images representing frames of a video,
 	turn each frame into a 1D distance signal and concatenate into an
 	image.
 	'''
-	pass
+	img = cv2.imread(img_path,0)
+	denoised = denoiseSilhouette(img)
+	cx, cy, cnt = findMaxContour(denoised)
+	dists = np.zeros(len(cnt))
+
+	#Find distance betweem center to all contour poiints
+	for i in xrange(len(cnt)):
+		x = cnt[i][0][0]
+		y = cnt[i][0][1]
+		dists[i] = np.sqrt(((x-cx)**2)+((y-cy)**2))
+
+	#L1 normalization of distances
+	dists_norm = dists / LA.norm(dists,1)
+
+	#Subsample the vector to a constant size
+	indices = np.linspace(0,len(dists_norm),360,endpoint=False,dtype=np.int32)
+	sampled_dists = dists_norm[indices]
+	return sampled_dists
+
+def loadData(datapath,dataset):
+	'''
+	MOBO path form:
+	/data/moboBgSub/userID/mode/vidID/orientation/frame.pbm
+
+	CASIA path form:
+	/DatasetB/silhouettes/subjectID/walkingStatus-seqNum/viewAngle
+
+	CASIA has more data to train on so it's reccomended to use that dataset when training
+	'''
+	N = 100   #The number of frames from each sequence to use
+	if(dataset=="MOBO"):
+		topLevel = datapath+"/moboBgSub/"
+		for userID in next(os.walk(topLevel))[1]:
+			#for mode in next(os.walk(topLevel+userID))[1]:
+			mode = "slowWalk"
+			for orientation in next(os.walk(topLevel+userID+"/"+"slowWalk"))[1]:
+				n = 0
+				for img in sorted(next(os.walk(topLevel+userID+"/"+mode+"/"+orientation))[2]):
+					if(n >= N):
+						continue
+					img_path = topLevel+userID+"/"+mode+"/"+orientation+"/"+img
+					distSignal = toDistSeq(img_path)
+					print img_path
+					n += 1
+					
+
+	elif(dataset=="CASIA"):
+		topLevel = datapath+"/silhouettes/"
+		for subID in next(os.walk(topLevel))[1]:
+			for seqNum in next(os.walk(topLevel+subID))[1]:
+				for viewAngle in next(os.walk(topLevel+subID+"/"+seqNum))[1]:
+					for img in next(os.walk(topLevel+subID+"/"+seqNum+"/"+viewAngle))[2]:
+						img_path = topLevel+subID+"/"+seqNum+"/"+viewAngle+"/"+img
+						distSignal = toDistSeq(img_path)
+	else:
+		print "Invalid dataset input"
+		return 0
+	return 0
 
 class GaitClassifier(object):
 	def __init__(self):
@@ -60,3 +122,5 @@ class GaitClassifier(object):
 
 	def predict(self,X):
 		pass
+
+loadData('data','MOBO')
